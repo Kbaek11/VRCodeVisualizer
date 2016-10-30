1923bc27625c3d38d5fdaac283abbec1aa59c863
cowtongue <revisedboomerguy@gmail.com>
2016-10-29 20:22:37 -0400

    new

--- VRCodeVisualizer/Assets/TestText.cs
+++ VRCodeVisualizer/Assets/TestText.cs
@@ -6,15 +6,19 @@ public class TestText : MonoBehaviour {
     public Transform canvas_t;
     public Color textColor;
     public Font aFont;
-    public string someStr = "test";
+    public string someStr;
     private GameObject textFunction; // 
-    private int numOfTexts = 5; // this will be # of functions
-	// Use this for initialization
-	void Start () {
+    private int numOfTexts = 4; // this will be # of functions
+    float[] xCoords = { 0, 0, (float)9.9, (float)-9.9}; // need new data 
+    float[] yCoords = { (float)-1.7, (float)-1.7, (float)-1.7, (float)-1.7 }; // need new data 
+    float[] zCoords = { (float)9.9, (float)-9.9, 0, 0}; // need new data 
+    float[] xRot = { 0, 0, 0, 0 }; // need new data 
+    float[] yRot = { 0, 180, 90, -90 }; // need new data 
+    float[] zRot = { 0, 0, 0, 0 }; // need new data 
+    void Start () {
         for (int i = 0; i < numOfTexts; i++)
         {
-            textFunction = CreateText(canvas_t, 0, 18, 0, "test", 20, aFont, textColor);
-            //Instantiate(textFunction);
+            textFunction = CreateText(canvas_t, xCoords[i], yCoords[i], zCoords[i], xRot[i], yRot[i], zRot[i], someStr, 20, aFont, textColor);
         }
     }
 	
@@ -23,14 +27,16 @@ public class TestText : MonoBehaviour {
 	    
 	}
 
-    GameObject CreateText(Transform canvas_transform, float x, float y, float z, string text_to_print, int font_size, Font someFont, Color text_color)
+    GameObject CreateText(Transform canvas_transform, float x, float y, float z, float rot_x, float rot_y, float rot_z, string text_to_print, int font_size, Font someFont, Color text_color)
     {
         GameObject UItextGO = new GameObject("Text2");
         UItextGO.transform.SetParent(this.transform);
 
         RectTransform trans = UItextGO.AddComponent<RectTransform>();
+        trans.sizeDelta = new Vector2(200, 200);
         trans.anchoredPosition3D = new Vector3(x, y, z);
-        trans.localScale = new Vector3(1, 1, 1);
+        trans.localScale = new Vector3((float)0.1, (float)0.1, (float)0.1);
+        trans.localRotation = new Quaternion(rot_x, rot_y, rot_z, 0);
 
         Text text = UItextGO.AddComponent<Text>();
         text.text = text_to_print;
